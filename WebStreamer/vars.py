4a1d5226c86b52fdfa90732b18a1ce5d8847976b
Tushar Patil <86295965+PrinceStarLord@users.noreply.github.com>
2022-12-17 00:56:24 -0600

    Update vars.py

--- WebStreamer/vars.py
+++ WebStreamer/vars.py
@@ -8,20 +8,20 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(environ.get("API_ID"))
-    API_HASH = str(environ.get("API_HASH"))
-    BOT_TOKEN = str(environ.get("BOT_TOKEN"))
+    API_ID = int(environ.get("API_ID","5291894"))
+    API_HASH = str(environ.get("API_HASH","e15ad5a64cb8bd39b2be463d0314295f"))
+    BOT_TOKEN = str(environ.get("BOT_TOKEN","5822788845:AAEE5dF911nHD17eunrYVJSLJWWG0L7orw0"))
     SLEEP_THRESHOLD = int(environ.get("SLEEP_THRESHOLD", "60"))  # 1 minte
     WORKERS = int(environ.get("WORKERS", "6"))  # 6 workers = 6 commands at once
     BIN_CHANNEL = int(
-        environ.get("BIN_CHANNEL", None)
+        environ.get("BIN_CHANNEL", "-1001741024715")
     )  # you NEED to use a CHANNEL when you're using MULTI_CLIENT
     PORT = int(environ.get("PORT", 8080))
     BIND_ADDRESS = str(environ.get("WEB_SERVER_BIND_ADDRESS", "0.0.0.0"))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    HAS_SSL = environ.get("HAS_SSL", False)
+    HAS_SSL = environ.get("HAS_SSL", True)
     HAS_SSL = True if str(HAS_SSL).lower() == "true" else False
-    NO_PORT = environ.get("NO_PORT", False)
+    NO_PORT = environ.get("NO_PORT", True)
     NO_PORT = True if str(NO_PORT).lower() == "true" else False
     if "DYNO" in environ:
         ON_HEROKU = True
@@ -29,7 +29,7 @@ class Var(object):
     else:
         ON_HEROKU = False
     FQDN = (
-        str(environ.get("FQDN", BIND_ADDRESS))
+        str(environ.get("FQDN", "165.140.241.42"))
         if not ON_HEROKU or environ.get("FQDN")
         else APP_NAME + ".herokuapp.com"
     )
@@ -40,12 +40,12 @@ class Var(object):
             "s" if HAS_SSL else "", FQDN, "" if NO_PORT else ":" + str(PORT)
         )
 
-    DATABASE_URL = str(environ.get('DATABASE_URL'))
-    UPDATES_CHANNEL = str(environ.get('UPDATES_CHANNEL', "aredirect"))
+    DATABASE_URL = str(environ.get('DATABASE_URL',"mongodb+srv://renamev3-1:renamev3-1@cluster0.hyv9toa.mongodb.net/?retryWrites=true&w=majority"))
+    UPDATES_CHANNEL = str(environ.get('UPDATES_CHANNEL', "OTSeries"))
     PAGE_LINK = environ.get('PAGE_LINK', None)
-    OWNER_ID = int(environ.get('OWNER_ID', '777000'))
+    OWNER_ID = int(environ.get('OWNER_ID', '1132901778'))
     SESSION_NAME = str(environ.get('SESSION_NAME', 'F2LxBot'))
-    FORCE_UPDATES_CHANNEL = environ.get('FORCE_UPDATES_CHANNEL', False)
+    FORCE_UPDATES_CHANNEL = environ.get('FORCE_UPDATES_CHANNEL', True)
     FORCE_UPDATES_CHANNEL = True if str(FORCE_UPDATES_CHANNEL).lower() == "true" and UPDATES_CHANNEL != 'aredirect' else False
 
     BANNED_CHANNELS = list(set(int(x) for x in str(environ.get("BANNED_CHANNELS", "-1001296894100")).split()))
